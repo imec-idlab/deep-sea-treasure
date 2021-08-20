da0e4eb1cc934a3d8e9dd73f625fd87e5c9ecc36
Thomas Cassimon <thomas.cassimon@uantwerpen.be>
2021-08-20 15:16:33 +0200

    Updated test suite to reflect removed build number injection

--- tests/test_version.py
+++ tests/test_version.py
@@ -11,11 +11,7 @@ import deep_sea_treasure
 class VersionTest(unittest.TestCase):
 	def test_build_no_injection(self):
 		# We are running in a CI Pipeline
-		if "CI_PIPELINE_ID" in os.environ:
-			self.assertTrue(deep_sea_treasure.__version__.VERSION.endswith(str(os.environ["CI_PIPELINE_ID"])))
-			self.assertRegex(deep_sea_treasure.__version__.VERSION, "\\d+\\.\\d+\\.\\d+\\+\\d+")
-		else:
-			self.assertRegex(deep_sea_treasure.__version__.VERSION, "\\d+\\.\\d+\\.\\d+")
+		self.assertRegex(deep_sea_treasure.__version__.VERSION, "\\d+\\.\\d+\\.\\d+")
 
 	def test_commit_injection(self):
 		# We are running in a CI Pipeline
