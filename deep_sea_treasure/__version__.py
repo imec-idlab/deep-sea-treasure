66c2d8d6999d2db38ee72bf5a415e5b51281b189
Thomas Cassimon <thomas.cassimon@uantwerpen.be>
2021-09-01 18:02:54 +0200

    Version bump.

--- deep_sea_treasure/__version__.py
+++ deep_sea_treasure/__version__.py
@@ -2,5 +2,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-VERSION: str = "2.0.1"
+VERSION: str = "1.1.0"
 COMMIT_HASH: str = ""
