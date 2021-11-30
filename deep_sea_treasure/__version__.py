14714764e34cf3336101660b13fce7c4bd3415e4
reinout <BeepBoop>
2021-11-30 13:48:44 +0100

    Version bump

--- deep_sea_treasure/__version__.py
+++ deep_sea_treasure/__version__.py
@@ -2,5 +2,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-VERSION: str = "2.0.0"
+VERSION: str = "2.0.1"
 COMMIT_HASH: str = ""
